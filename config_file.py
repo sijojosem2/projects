e999adcad9532ef7104bd22f2b8897c8a0b8e669
sijoosje <39704621+sijoosje@users.noreply.github.com>
2021-08-25 03:35:10 +0200

    reset with url

--- config_file.py
+++ config_file.py
@@ -4,7 +4,7 @@
 ENV_CONFIG={
 
     'DB_URI':'sqlite:///main_loc',
-    'GET_STATS_URL':'https://https://mc-simple-req.herokuapp.com/get_stats_results',
-    'GET_HOME_URL':'https://https://mc-simple-req.herokuapp.com/home',
+    'GET_STATS_URL':'https://mc-simple-req.herokuapp.com/get_stats_results',
+    'GET_HOME_URL':'https://mc-simple-req.herokuapp.com/home',
     'TABLE_NAME':'mc_locations'
 }
